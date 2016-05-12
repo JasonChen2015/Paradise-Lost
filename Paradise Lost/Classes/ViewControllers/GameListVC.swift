088c6be6a33adff21e8e836db398fd25a4ac4154
JasonChen2015 <cyl1234567890@163.com>
2016-05-12 21:36:07 +0800

    (\ /) (O.o) (> <)  Bunny approves theses changes.

---
+++ Paradise Lost/Classes/ViewControllers/GameListVC.swift
@@ -0,0 +1,31 @@
+//
+//  GameListVC.swift
+//  Paradise Lost
+//
+//  Created by Jason Chen on 5/12/16.
+//  Copyright © 2016 Jason Chen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class GameListVC: UniversalTableViewController {
+
+    // MARK: life cycle
+
+    override func viewDidLoad() {
+        viewName = "Game List"
+        itemNames = ["2048", "Sudoku", "Snake"]
+
+        super.viewDidLoad()
+    }
+
+    // MARK: event response
+
+    override func cellButtonAction(cell: UITableViewCell) {
+        // TODO:
+        if let indexPath = tableView.indexPathForCell(cell) {
+            print("\(indexPath.row)")
+        }
+    }
+}
---
+++ Paradise Lost/Classes/ViewControllers/ToolListVC.swift
@@ -0,0 +1,31 @@
+//
+//  ToolListVC.swift
+//  Paradise Lost
+//
+//  Created by Jason Chen on 5/12/16.
+//  Copyright © 2016 Jason Chen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ToolListVC: UniversalTableViewController {
+
+    // MARK: life cycle
+
+    override func viewDidLoad() {
+        viewName = "Tool List"
+        itemNames = ["Bar Code Scan", "Test"]
+        
+        super.viewDidLoad()
+    }
+
+    // MARK: event response
+
+    override func cellButtonAction(cell: UITableViewCell) {
+        if let indexPath = tableView.indexPathForCell(cell) {
+            // TODO:
+            print("\(indexPath.row)")
+        }
+    }
+}
