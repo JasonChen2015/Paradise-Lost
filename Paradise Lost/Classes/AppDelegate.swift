6df70dbb6d93aad88f7a305c56fa47a22b3ae81a
JasonChen2015 <cyl1234567890@163.com>
2016-07-28 10:47:12 +0800

    add security

--- Paradise Lost/Classes/AppDelegate.swift
+++ Paradise Lost/Classes/AppDelegate.swift
@@ -50,6 +50,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+        
+        // To hide screen shot when entering background due to security
+        UIApplication.sharedApplication().keyWindow?.hidden = true
     }
 
     func applicationDidEnterBackground(application: UIApplication) {
@@ -59,6 +62,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        
+        // resume the keyWindow and show it to user
+        UIApplication.sharedApplication().delegate?.window!!.hidden = false
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
